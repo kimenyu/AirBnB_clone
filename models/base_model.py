3aad592885dd8ac0cb680ecc60e78e11b43642c0
kimenyu <njorogekimenyu@gmail.com>
2023-05-10 03:42:49 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -9,10 +9,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Public instance attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
     def save(self):
         """updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
