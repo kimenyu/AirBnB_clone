f67fa16f4ed772d7c3bf5a771fc92800ba70de97
kimenyu <njorogekimenyu@gmail.com>
2023-06-19 15:39:41 +0000

    base

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/pthon3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """a base clas for all hbnb models  """
+    def __init__(self, *args, **kwargs):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
+    def save(self):
+        self.updated_at = datetime.now()
+    
+    def to_dict(self):
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
